bb753d2ed96740ce4cf162f6f9a241408d5ca335
Matt Jared <mattjared9@gmail.com>
2014-05-21 14:47:20 -0500

    Removing spaces and unecessary functions

--- search.php
+++ search.php
@@ -7,18 +7,13 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
+	<section class="container">
 		<?php if ( have_posts() ) : ?>
-
 			<header class="page-header">
 				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'beans' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
-
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
-
 				<?php
 				/**
 				 * Run the loop for the search to output the results.
@@ -27,19 +22,10 @@ get_header(); ?>
 				 */
 				get_template_part( 'content', 'search' );
 				?>
-
 			<?php endwhile; ?>
-
 			<?php beans_paging_nav(); ?>
-
 		<?php else : ?>
-
 			<?php get_template_part( 'content', 'none' ); ?>
-
 		<?php endif; ?>
-
-		</main><!-- #main -->
 	</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
