acb2b2bf4e04ef5a0905c27fadc372118214c6e3
Matt Jared <mattjared9@gmail.com>
2014-09-20 13:31:04 -0500

    Google fonts, header structure, styling nav

--- functions.php
+++ functions.php
@@ -20,8 +20,7 @@ if ( ! function_exists( 'beans_setup' ) ) :
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function beans_setup() {
-
+function beans_setup() {\
 	/*
 	 * Make theme available for translation.
 	 * Translations can be filed in the /languages/ directory.
@@ -94,11 +93,15 @@ function beans_scripts() {
 
 	wp_enqueue_script( 'beans-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Dosis:400,700|Cantarell:400,700,400italic,700italic');
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'beans_scripts' );
+
+
 
 /**
  * Implement the Custom Header feature.
